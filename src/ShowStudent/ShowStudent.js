4a9370ac0320138a2d45fc1b191d2f8a1d8b6088
saurabhdharamkar <saurabhdharamkar2714@gmail.com>
2022-01-24 14:55:46 -0800

    commit

--- src/ShowStudent/ShowStudent.js
+++ src/ShowStudent/ShowStudent.js
@@ -2,8 +2,38 @@ import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { NavLink } from "react-router-dom";
 import { showStudents } from "../Actions/StudentAction";
+import { styled } from '@mui/material/styles';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell, { tableCellClasses } from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import Button from "@material-ui/core/Button";
+
 
 export const ShowStudent = () => {
+  const StyledTableCell = styled(TableCell)(({ theme }) => ({
+    [`&.${tableCellClasses.head}`]: {
+      backgroundColor: theme.palette.common.black,
+      color: theme.palette.common.white,
+    },
+    [`&.${tableCellClasses.body}`]: {
+      fontSize: 14,
+    },
+  }));
+  
+  const StyledTableRow = styled(TableRow)(({ theme }) => ({
+    '&:nth-of-type(odd)': {
+      backgroundColor: theme.palette.action.hover,
+    },
+    // hide last border
+    '&:last-child td, &:last-child th': {
+      border: 0,
+    },
+  }));
+  
 
     let studAr = useSelector((store) => store.StudentReducer)
     let dispatch = useDispatch();
@@ -14,39 +44,48 @@ export const ShowStudent = () => {
 
 
     return (
-        <div >
-            <table className="mt-3 table table-warning table-striped">
-                <thead>
-                    <tr>
-                        <th>ID</th>
-                        <th>Name</th>
-                        <th>Department</th>
-                        <th>Year</th>
-                        <th>Action</th>
-                        
-                    </tr>
-                </thead>
-                {
-                    studAr.map((student) => {
+         <div >
+           <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 700 }} aria-label="customized table">
+        <TableHead>
+          <TableRow>
+            <StyledTableCell>Dessert (100g serving)</StyledTableCell>
+            <StyledTableCell align="right">Calories</StyledTableCell>
+            <StyledTableCell align="right">Fat&nbsp;(g)</StyledTableCell>
+            <StyledTableCell align="right">Carbs&nbsp;(g)</StyledTableCell>
+            <StyledTableCell align="right">Protein&nbsp;(g)</StyledTableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {studAr.map((row) => (
+            <StyledTableRow key={row.name}>
+              <StyledTableCell component="th" scope="row">
+                {row.name}
+              </StyledTableCell>
+              <StyledTableCell align="right">{row.id}</StyledTableCell>
+              <StyledTableCell align="right">{row.department}</StyledTableCell>
+              <StyledTableCell align="right">{row.year}</StyledTableCell>
+              <StyledTableCell align="right">
+              <NavLink to={`/student/update/${row.id}`}>
+              <Button variant="contained">Update</Button>
 
-                        return (
+              </NavLink>
+              <NavLink to={`/students/delete/${row.id}`}>
+              <Button variant="contained" style={{marginLeft:'5px'}}>Delete</Button>
 
-                            <tbody >
-                                <tr key={student.id}>
-                                    <td>{student.id}</td>
-                                    <td>{student.name}</td>
-                                    <td>{student.department}</td>
-                                    <td>{student.year}</td>
-                                    <td>
-                                        <NavLink to={`/student/update/${student.id}`}><button className="btn btn-warning">Update</button></NavLink>
-                                        <NavLink to={`/students/delete/${student.id}`}><button className="btn btn-danger mx-2">Delete</button></NavLink>
-                                    </td>
-                                </tr>
-                            </tbody>
-                        );
-                    })
-                }
-            </table>
-        </div>
+              </NavLink>
+              </StyledTableCell>
+              
+               <StyledTableCell align="right" ></StyledTableCell>
+               
+
+              <StyledTableCell ></StyledTableCell>
+            </StyledTableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>       </div> 
+
+        
     )
 }
